c39bd8888b7b6257cfefbf5d24c26f0db5ab9896
PL23006\Tokunaga <n-tokunaga@plaice.co.jp>
2023-11-13 16:54:44 +0900

    更新

---
+++ server.js
@@ -0,0 +1,90 @@
+// const http = require("http");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+    const filePath = path.join(__dirname, "index.html");
+    res.sendFile(filePath);
+});
+
+app.get("/:file", (req, res) => {
+    const filePath = path.join(__dirname, req.params.file);
+    res.sendFile(filePath);
+});
+
+function sendFile(res, filePath){
+    fs.readFile(filePath, (err, data) => {
+        if(!err){
+            let contentType = "text/html";
+            if(filePath.endsWith(".css")){
+                contentType = "text/css";
+            }
+            res.writeHead(200, {"content-Type": contentType});
+            res.end(data);
+            console.log(`Sent content to the client: ${filePath}`);
+        }else{
+            res.writeHead(404, { "Content-Type": "text/html"});
+            res.end("<h1>File not found</h1>");
+        }
+    });
+}
+
+// const  server = http.createServer((request, response) => {
+//     let filePath = path.join(__dirname, "研修", request.url === "/" ? "index.html" : request.url);
+
+//     fs.readFile(filePath, (err, data) => {
+//         if(!err){
+//             let contentType = "text/html";
+//             if(filePath.endsWith(".css")){
+//                 contentType = "text/css";
+//             }
+//             response.writeHead(200, { "Content-Type": contentType});
+//             response.end(data);
+//             console.log(`Sent content to the client: ${request.url}`);
+//         }else{
+//             response.writeHead(404, { "Content-Type": "text/html"});
+//             response.end("<h1>File not found</h1>");
+//         }
+//     });
+// });
+
+// server.listen(port);
+// console.log(`The server has started and is listening on port number: ${port}`);
+
+app.listen(port, () => {
+    console.log(`The server has started and is listening on port number: ${port}`);
+})
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+
+// const port = 3000;
+
+// const  server = http.createServer((request, response) => {
+//     // let filePath = path.join(__dirname, "研修", request.url === "/" ? "index.html" : request.url);
+//   let filePath = path.join(__dirname, request.url === "/" ? "index.html" : request.url);
+  
+//     fs.readFile(filePath, (err, data) => {
+//         if(!err){
+//             let contentType = "text/html";
+//             if(filePath.endsWith(".css")){
+//                 contentType = "text/css";
+//             }
+//             response.writeHead(200, { "Content-Type": contentType});
+//             response.end(data);
+//             console.log(`Sent content to the client: ${request.url}`);
+//         }else{
+//             response.writeHead(404, { "Content-Type": "text/html"});
+//             response.end("<h1>File not found</h1>");
+//         }
+//     });
+// });
+
+// server.listen(port);
+// console.log(`The server has started and is listening on port number: ${port}`);
